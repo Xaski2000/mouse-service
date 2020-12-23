9fe4cfa1b6208263fe18757ad580161414fc8ef3
Ivan Shashkov <mr.xaski.2002@gmail.com>
2020-12-24 00:57:55 +0700

    Start

---
+++ php/send-bot.php
@@ -0,0 +1,28 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta  http-equiv  = "refresh" content = "0, URL=..\index.html" >
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    //Получаем данные из глобальной переменной $_GET, так как мы передаем данные методом GET
+    $name = $_GET['name']; // Вытаскиваем имя в переменную
+    $number = $_GET['number']; // Вытаскиваем почту в переменную
+    $rating = $_GET['rating'];
+    $review = $_GET['review'];
+    $message =
+    Номер телефона: $number <br>
+    Оценка: $rating <br>
+    Отзыв: $review"; // Формируем сообщение, отправляемое на почту
+    $to = "xaski_2000@mail.ru"; // Задаем получателя письма
+    $from = "noreply-site.web.cofp.ru"; // От кого пришло письмо
+    $subject = "Отзыв с вашего сайта"; // Задаем тему письма
+    $headers = "From: $from\r\nReply-To: $to\r\nContent-type: text/html; charset=utf-8\r\n"; // Формируем заголовок письма (при неправильном формировании может ломаться кодировка и т.д.)
+    mail($to, $subject, $message,
+    "Имя: $name <br> $headers)
+?>
+</body>
+</html>
+
---
+++ php/send-top.php
@@ -0,0 +1,30 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta  http-equiv  = "refresh" content = "0, URL=..\index.html" >
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    //Получаем данные из глобальной переменной $_GET, так как мы передаем данные методом GET
+    $name = $_GET['name']; // Вытаскиваем имя в переменную
+    $number = $_GET['number']; // Вытаскиваем почту в переменную
+    $adress = $_GET['adress'];
+    $system = $_GET['system'];
+    $problem = $_GET['problem'];
+    $message =
+    "Имя: $name <br>
+    Номер телефона: $number <br>
+    Адрес: $adress <br>
+    Система: $system <br>
+    Проблема: $problem"; // Формируем сообщение, отправляемое на почту
+    $to = "xaski_2000@mail.ru"; // Задаем получателя письма
+    $from = "noreply-site.web.cofp.ru"; // От кого пришло письмо
+    $subject = "Заказ с вашего сайта"; // Задаем тему письма
+    $headers = "From: $from\r\nReply-To: $to\r\nContent-type: text/html; charset=utf-8\r\n"; // Формируем заголовок письма (при неправильном формировании может ломаться кодировка и т.д.)
+    mail($to, $subject, $message, $headers)
+?>
+</body>
+</html>
+
